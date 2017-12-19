0322c30b33458aa38b6b8bd729025415ecf98022
taboo09 <34669753+taboo09@users.noreply.github.com>
2017-12-19 04:28:19 +0000

    Update nQueens.py

--- nQueens.py
+++ nQueens.py
@@ -68,3 +68,17 @@ class Board(object):
 board = Board(8)
 
 board.nQueens()
+
+
+#solution is:
+
+#      0  1  2  3  4  5  6  7
+#    0 Q  -  -  -  -  -  -  -
+#    1 -  -  -  -  -  -  Q  -
+#    2 -  -  -  -  Q  -  -  -
+#    3 -  -  -  -  -  -  -  Q
+#    4 -  Q  -  -  -  -  -  -
+#    5 -  -  -  Q  -  -  -  -
+#    6 -  -  -  -  -  Q  -  -
+#    7 -  -  Q  -  -  -  -  -
+
